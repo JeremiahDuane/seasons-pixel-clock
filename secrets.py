6ca77ba89118b08fbd747e335a5f3ac0dd7bf7e6
= <=>
2022-07-22 20:03:43 -0500

    Mass merge from matrixportal

---
+++ secrets.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : '<YOUR_SSID>',
+    'password' : '<YOUR_PASSWORD>',
+    'timezone' : '<YOUR_TIMEZONE>',
+    'aio_username' : '<YOUR_AIO_USERNAME>',
+    'aio_key' : '<YOUR_AIO_KEY>',
+    'api_read-unread' : '<YOUR_API>',
+    }
