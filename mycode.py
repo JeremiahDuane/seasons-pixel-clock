bb35ed5b7961a23faff4829ad527cfc83370c83b
= <=>
2022-07-24 11:01:02 -0500

    Updates

--- mycode.py
+++ mycode.py
@@ -2,7 +2,8 @@
 import time
 import sys
 
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from datetime import datetime, timedelta
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image
 
 if len(sys.argv) < 2:
@@ -26,10 +27,24 @@ matrix = RGBMatrix(options = options)
 image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
 matrix.SetImage(image.convert('RGB'))
+offscreen_canvas = matrix.CreateFrameCanvas()
+
+font = graphics.Font()
+font.LoadFont("/fonts/pixelclock-main-24.bdf") 
+now = datetime.now()
+current_time = now.strftime("%H")    
+color_time = graphics.Color(200, 160, 15)
+graphics.DrawText(offscreen_canvas, font, 1, 7, color_time ,current_time)
+
+
+offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+time.sleep(.005)
+offscreen_canvas.Clear()
 
 try:
     print("Press CTRL-C to stop.")
     while True:
         time.sleep(100)
+
 except KeyboardInterrupt:
     sys.exit(0)
