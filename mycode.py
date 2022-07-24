7139bf7c087a1ea347e6428ae2ede0b109c76b49
= <=>
2022-07-24 16:40:21 -0500

    Updates

--- mycode.py
+++ mycode.py
@@ -94,7 +94,7 @@ def getScene(year, month, day, weekday):
     scenes = [
         Scene(RGB(255,255,255), RGB(255,255,255), "./bmps/blank.bmp", "./bmps/blank.bmp"),                 # -0-  Notification 
         Scene(RGB(255,255,255), RGB(0,255,255), "./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"),         # -1-  January Snowman
-        Scene(RGB(255,190,0), RGB(255,150,0), "./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"),     # -8-  August Sunflowers
+        Scene(RGB(200,190,0), RGB(255,190,0), "./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"),     # -8-  August Sunflowers
         Scene(RGB(255,255,255), RGB(0,255,255), "./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp")            # -12- December Wreath
     ]
     
