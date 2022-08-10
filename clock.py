6584b4d7a8532f9c5587c2b2b4666f3dcceb453e
= <=>
2022-08-09 20:56:36 -0500

    Updates

--- clock.py
+++ clock.py
@@ -84,7 +84,7 @@ def getScene(year, month, day, weekday):
 #---------- Countdown ----------#
 def getCountdownCanvas(cvsClock, year, month, day, hour, minute, second, weekday, count):
     #Clock
-    strTime = getCountdownString(count.days, count.hours, count.minutes)
+    strTime = getCountdownString(count.day, count.hour, count.minute)
     scene = getScene(year, month, day, weekday)
 
     #Scene
