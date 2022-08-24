f9c927945592d911c32ae0d8e1e9f167868e4eaa
= <=>
2022-08-24 01:43:51 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -37,8 +37,8 @@ SCENES = [
     Scene(RGB(255,255,255), RGB(255,255,255), "./bmps/blank.bmp", "./bmps/blank.bmp"),                              # -0-  Notification 
     Scene(RGB(255,255,255), RGB(0,255,255), "./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"),                        # -1-  Snowman
     Scene(RGB(255,190,0), RGB(255,100,0), "./bmps/scarecrow_1.bmp", "./bmps/scarecrow_2.bmp"),                      # -8-  Scarecrow
-    Scene(RGB(200,190,0), RGB(255,190,0), "./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"),                      # -8-  Sunflowers
     Scene(RGB(255,190,0), RGB(255,100,0), "./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"),                          # -8-  Pumpkin
+    Scene(RGB(200,190,0), RGB(255,190,0), "./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"),                      # -8-  Sunflowers
     Scene(RGB(255,255,255), RGB(255,150,255), "./bmps/pinkcake_1.bmp", "./bmps/pinkcake_2.bmp", BirthdayAction),    # -8-  Birthday
     Scene(RGB(255,255,255), RGB(0,255,255), "./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp")                           # -12- Wreath
 ]
