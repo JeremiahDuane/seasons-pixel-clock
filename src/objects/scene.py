8fca2db14a9da5f3e4fd7608fda576b7feb5d26e
= <=>
2022-08-25 23:28:44 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -36,7 +36,7 @@ def BirthdayAction(graphics, canvas, font, clrPrimary, clrSecondary, year):
 SCENES = [
     Scene(RGB(255,255,255), RGB(255,255,255), ["./bmps/blank.bmp", "./bmps/blank.bmp"], 1),                                                                                # -0-  Notification 
     Scene(RGB(255,190,0), RGB(255,100,0), ["./bmps/scarecrow_1.bmp", "./bmps/scarecrow_2.bmp"], 1),                                                                        # -8-  Scarecrow
-    Scene(RGB(255,190,0), RGB(255,180,255), ["./bmps/daffodil_1.bmp", "./bmps/daffodil_2.bmp"], 1),                                                                            # -8-  Pumpkin
+    Scene(RGB(255,255,255), RGB(255,180,255), ["./bmps/daffodil_1.bmp", "./bmps/daffodil_2.bmp"], 1),                                                                            # -8-  Pumpkin
     Scene(RGB(255,190,0), RGB(255,130,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"], 1),                                                                            # -8-  Pumpkin
     Scene(RGB(255,255,255), RGB(245,100,255), ["./bmps/pinkcake_1.bmp", "./bmps/pinkcake_2.bmp", "./bmps/pinkcake_3.bmp", "./bmps/pinkcake_4.bmp"], 4, BirthdayAction),    # -8-  Birthday
     Scene(RGB(200,190,0), RGB(255,190,0), ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"], 1),                                                                        # -8-  Sunflowers
