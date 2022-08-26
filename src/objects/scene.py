d0b931af5f02bb30d9e67fc002ad083dd41f2475
= <=>
2022-08-25 20:41:17 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -4,7 +4,7 @@ class Scene:
     def __init__(self, primaryColor, secondaryColor, bmps, action=None):      
         self.primaryColor = primaryColor            
         self.secondaryColor = secondaryColor            
-        self.bmp = bmps                                  
+        self.bmps = bmps                                  
         self.action = action   
     def setBMPs(self, bmps):
         self.bmps = bmps
@@ -31,9 +31,9 @@ def BirthdayAction(graphics, canvas, font, clrPrimary, clrSecondary, year):
 SCENES = [
     Scene(RGB(255,255,255), RGB(255,255,255), ["./bmps/blank.bmp", "./bmps/blank.bmp"]),                              # -0-  Notification 
     Scene(RGB(255,190,0), RGB(255,100,0), ["./bmps/scarecrow_1.bmp", "./bmps/scarecrow_2.bmp"]),                      # -8-  Scarecrow
+    Scene(RGB(255,190,0), RGB(255,130,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"]),                          # -8-  Pumpkin
     Scene(RGB(255,255,255), RGB(245,100,255), ["./bmps/pinkcake_1.bmp", "./bmps/pinkcake_2.bmp"], BirthdayAction),    # -8-  Birthday
     Scene(RGB(200,190,0), RGB(255,190,0), ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"]),                      # -8-  Sunflowers
-    Scene(RGB(255,190,0), RGB(255,130,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"]),                          # -8-  Pumpkin
     Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp"]),                          # -12- Wreath
     Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"]),                        # -1-  Snowman
 ]
