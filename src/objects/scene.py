b097b789bcfbe57979d2662ef6b03c550db74b95
= <=>
2022-08-25 21:53:08 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -34,11 +34,11 @@ def BirthdayAction(graphics, canvas, font, clrPrimary, clrSecondary, year):
     graphics.DrawText(canvas, font, 47, 21, color, str(year-1997))
 
 SCENES = [
-    Scene(RGB(255,255,255), RGB(255,255,255), ["./bmps/blank.bmp", "./bmps/blank.bmp"], 2),                                                                                # -0-  Notification 
-    Scene(RGB(255,190,0), RGB(255,100,0), ["./bmps/scarecrow_1.bmp", "./bmps/scarecrow_2.bmp"], 2),                                                                        # -8-  Scarecrow
-    Scene(RGB(255,255,255), RGB(245,100,255), ["./bmps/pinkcake_1.bmp", "./bmps/pinkcake_2.bmp", "./bmps/pinkcake_3.bmp", "./bmps/pinkcake_4.bmp"], 1, BirthdayAction),    # -8-  Birthday
-    Scene(RGB(255,190,0), RGB(255,130,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"], 2),                                                                            # -8-  Pumpkin
-    Scene(RGB(200,190,0), RGB(255,190,0), ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"], 2),                                                                        # -8-  Sunflowers
-    Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp"], 2),                                                                            # -12- Wreath
-    Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"], 2),                                                                          # -1-  Snowman
+    Scene(RGB(255,255,255), RGB(255,255,255), ["./bmps/blank.bmp", "./bmps/blank.bmp"], 1),                                                                                # -0-  Notification 
+    Scene(RGB(255,190,0), RGB(255,100,0), ["./bmps/scarecrow_1.bmp", "./bmps/scarecrow_2.bmp"], 1),                                                                        # -8-  Scarecrow
+    Scene(RGB(255,255,255), RGB(245,100,255), ["./bmps/pinkcake_1.bmp", "./bmps/pinkcake_2.bmp", "./bmps/pinkcake_3.bmp", "./bmps/pinkcake_4.bmp"], 4, BirthdayAction),    # -8-  Birthday
+    Scene(RGB(255,190,0), RGB(255,130,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"], 1),                                                                            # -8-  Pumpkin
+    Scene(RGB(200,190,0), RGB(255,190,0), ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"], 1),                                                                        # -8-  Sunflowers
+    Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp"], 1),                                                                            # -12- Wreath
+    Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"], 1),                                                                          # -1-  Snowman
 ]
