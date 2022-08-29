bdf6f685a6eaa90bbd33f9a8c3421a2f0b019fc7
= <=>
2022-08-28 20:21:49 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -47,5 +47,5 @@ SCENES = [
         "./bmps/egg_4.bmp", "./bmps/egg_4.bmp", "./bmps/egg_4.bmp", "./bmps/egg_4.bmp", 
         "./bmps/egg_3.bmp", "./bmps/egg_2.bmp", "./bmps/egg_1.bmp", "./bmps/egg_1.bmp",
         ], 1),                                                                                                                                                             # -8-  Egg
-    Scene(RGB(255,50,80), RGB(255, 255, 0) ["./bmps/turkey_1.bmp", "./bmps/turkey_2.bmp", "./bmps/turkey_3.bmp"], 1),                                                     # -9-  Turkey
+    Scene(RGB(255,50,80), RGB(255, 255, 0), ["./bmps/turkey_1.bmp", "./bmps/turkey_2.bmp", "./bmps/turkey_3.bmp"], 1),                                                     # -9-  Turkey
 ]
