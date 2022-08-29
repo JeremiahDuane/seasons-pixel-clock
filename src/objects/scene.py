55006d2b107d07986a3c98424bff3ef077d26cb5
= <=>
2022-08-29 00:47:29 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -50,7 +50,7 @@ SCENES = [
     Scene(RGB(255,255,255), RGB(255,180,255), RGB(255,255,255), RGB(255,218,15),["./bmps/daffodil_1.bmp", "./bmps/daffodil_2.bmp"], 4),                                                                             # -3-  Daffodil
     Scene(RGB(255,190,0), RGB(255,130,0),RGB(107,68,31), RGB(175,96,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"], 4),                                                                                      # -4-  Pumpkin
     Scene(RGB(200,190,0), RGB(255,150,0),RGB(128,64,0), RGB(0,96,0),  ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"], 4),                                                                                     # -5-  Sunflowers
-    Scene(RGB(0,115,0), RGB(255,0,0),RGB(107,68,31), RGB(0,0,255), ["./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp"], 4),                                                                                  # -6-  Wreath
+    Scene(RGB(0,180,0), RGB(255,0,0),RGB(0, 0, 255), RGB(107,68,31), ["./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp"], 4),                                                                                  # -6-  Wreath
     Scene(RGB(255,255,255), RGB(0,255,255),RGB(255,255,255), RGB(255,255,255), ["./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"], 4),                                                                                # -7-  Snowman
     Scene(RGB(255,255,255), RGB(245,160,255), RGB(255,255,120), RGB(160,245,255),
         ["./bmps/egg_1.bmp", "./bmps/egg_1.bmp", "./bmps/egg_2.bmp", "./bmps/egg_3.bmp", 
