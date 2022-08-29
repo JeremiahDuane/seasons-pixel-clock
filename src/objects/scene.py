0f07f9e338089a36c858a0e2eb1cb6d00a970e30
= <=>
2022-08-28 20:50:52 -0500

    Adding new BMPs and fonts

--- src/objects/scene.py
+++ src/objects/scene.py
@@ -44,7 +44,7 @@ SCENES = [
     Scene(RGB(255,255,255), RGB(245,100,255), ["./bmps/pinkcake_1.bmp", "./bmps/pinkcake_2.bmp", "./bmps/pinkcake_3.bmp", "./bmps/pinkcake_4.bmp"], 1, BirthdayAction),    # -2-  Birthday
     Scene(RGB(255,255,255), RGB(255,180,255), ["./bmps/daffodil_1.bmp", "./bmps/daffodil_2.bmp"], 4),                                                                      # -3-  Daffodil
     Scene(RGB(255,190,0), RGB(255,130,0), ["./bmps/pumpkin_1.bmp", "./bmps/pumpkin_2.bmp"], 4),                                                                            # -4-  Pumpkin
-    Scene(RGB(200,190,0), RGB(255,190,0), ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"], 4),                                                                        # -5-  Sunflowers
+    Scene(RGB(200,190,0), RGB(255,150,0), ["./bmps/sunflower_1.bmp", "./bmps/sunflower_2.bmp"], 4),                                                                        # -5-  Sunflowers
     Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/wreath_1.bmp", "./bmps/wreath_2.bmp"], 4),                                                                            # -6-  Wreath
     Scene(RGB(255,255,255), RGB(0,255,255), ["./bmps/snowman_1.bmp", "./bmps/snowman_2.bmp"], 4),                                                                          # -7-  Snowman
     Scene(RGB(255,255,255), RGB(255,255,0), 
@@ -52,5 +52,5 @@ SCENES = [
         "./bmps/egg_4.bmp", "./bmps/egg_4.bmp", "./bmps/egg_4.bmp", "./bmps/egg_4.bmp", 
         "./bmps/egg_3.bmp", "./bmps/egg_2.bmp", "./bmps/egg_1.bmp", "./bmps/egg_1.bmp",
         ], 1),                                                                                                                                                             # -8-  Egg
-    Scene(RGB(200,190,0), RGB(255,190,0), ["./bmps/turkey_1.bmp", "./bmps/turkey_2.bmp", "./bmps/turkey_3.bmp"], 2),                                                     # -9-  Turkey7
+    Scene(RGB(200,190,0), RGB(255,150,0), ["./bmps/turkey_1.bmp", "./bmps/turkey_2.bmp", "./bmps/turkey_3.bmp"], 2),                                                     # -9-  Turkey7
 ]
