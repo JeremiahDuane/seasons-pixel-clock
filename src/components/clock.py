5dca058ea766df7388f9f9e883745710b0a4439d
= <=>
2022-08-29 23:39:49 -0500

    Adding new BMPs and fonts

--- src/components/clock.py
+++ src/components/clock.py
@@ -343,5 +343,28 @@ def getScene():
 #---------- Shared ----------#
 TESTER = 0
 def TEST():
+    def isEaster(year, month, day):
+        y = year
+        # golden year - 1
+        g = y % 19
+        # offset
+        e = 0
+        # century
+        c = y/100
+        # h is (23 - Epact) mod 30
+        h = (c-c/4-(8*c+13)/25+19*g+15)%30
+        # number of days from March 21 to Paschal Full Moon
+        i = h-(h/28)*(1-(h/28)*(29/(h+1))*((21-g)/11))
+        # weekday for Paschal Full Moon (0=Sunday)
+        j = (y+y/4+i+2-c+c/4)%7
+        # number of days from March 21 to Sunday on or before Paschal Full Moon
+        # p can be from -6 to 28
+        p = i-j+e
+        d = 1+(p+27+(p+6)/40)%31
+        m = 3+(p+26)/30
+        return year == y and month == m and day == d
+    
+    print (isEaster(2022, 1, 1), isEaster(2022, 4, 17), isEaster(2022, 4, 16))
+
     global TESTER
     return SCENES[TESTER]
